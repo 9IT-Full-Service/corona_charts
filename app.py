b25f835088618394afd4c36c182af13f1118e840
Ruediger Küpper <ruediger@kuepper.nrw>
2020-09-30 20:53:04 +0200

    update

--- app.py
+++ app.py
@@ -88,14 +88,14 @@ def bar_x(id):
 def probes():
     line_labels=probelabels
     line_values=probevalues
-    return render_template('probes_line_chart.html', title='Corona Probes', max=37000, labels=line_labels, values=line_values)
+    return render_template('probes_line_chart.html', title='Corona Probes', max=38000, labels=line_labels, values=line_values)
 
 @app.route('/probes/<id>')
 def probes_x(id):
     id = int(id)
     line_labels=probelabels[-id:]
     line_values=probevalues[-id:]
-    return render_template('probes_line_chart.html', title='Corona Probes', max=37000, labels=line_labels, values=line_values)
+    return render_template('probes_line_chart.html', title='Corona Probes', max=38000, labels=line_labels, values=line_values)
 
 @app.route('/line')
 def line():
@@ -115,27 +115,27 @@ def line_x(id):
 def averagebar():
     bar_labels=averagelabels
     bar_values=averagevalues
-    return render_template('averagebar_chart.html', title='Corona 7-Tage Durchschnitt ', max=25, labels=bar_labels, values=bar_values)
+    return render_template('averagebar_chart.html', title='Corona 7-Tage Durchschnitt ', max=30, labels=bar_labels, values=bar_values)
 
 @app.route('/averageline')
 def averageline():
     line_labels=averagelabels
     line_values=averagevalues
-    return render_template('average_line_chart.html', title='Corona 7-Tage Durchschnitt', max=25, labels=line_labels, values=line_values)
+    return render_template('average_line_chart.html', title='Corona 7-Tage Durchschnitt', max=30, labels=line_labels, values=line_values)
 
 @app.route('/averagebar/<id>')
 def averagebar_x(id):
     id = int(id)
     bar_labels=averagelabels[-id:]
     bar_values=averagevalues[-id:]
-    return render_template('averagebar_chart.html', title='Corona 7-Tage Durchschnitt ', max=25, labels=bar_labels, values=bar_values)
+    return render_template('averagebar_chart.html', title='Corona 7-Tage Durchschnitt ', max=30, labels=bar_labels, values=bar_values)
 
 @app.route('/averageline/<id>')
 def averageline_x(id):
     id = int(id)
     line_labels=averagelabels[-id:]
     line_values=averagevalues[-id:]
-    return render_template('average_line_chart.html', title='Corona 7-Tage Durchschnitt', max=25, labels=line_labels, values=line_values)
+    return render_template('average_line_chart.html', title='Corona 7-Tage Durchschnitt', max=30, labels=line_labels, values=line_values)
 
 @app.route('/current')
 def current():
