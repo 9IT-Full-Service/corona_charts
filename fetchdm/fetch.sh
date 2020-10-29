55c62d0952c69e06d2b84555ee8d172566d33b5f
Ruediger Küpper <ruediger@kuepper.nrw>
2020-10-29 17:38:03 +0100

    update produktIDs, feutches Toilettenpapier raus.

--- fetchdm/fetch.sh
+++ fetchdm/fetch.sh
@@ -2,8 +2,8 @@
 
 DATUM=$(date +%Y-%m-%d-%H-%M-%S)
 
-curl -s -o /home/rpr/coronacharts/fetchdm/data/${DATUM}.json "https://products.dm.de/store-availability/DE/availability?dans=595420,708997,137425,28171,485698,799358,863567,452740,610544,846857,709006,452753,879536,452744,485695,853483,594080,504606,593761,525943,842480,535981,127048,524535&storeNumbers=2475"
-
+# curl -s -o /home/rpr/coronacharts/fetchdm/data/${DATUM}.json "https://products.dm.de/store-availability/DE/availability?dans=595420,708997,137425,28171,485698,799358,863567,452740,610544,846857,709006,452753,879536,452744,485695,853483,594080,504606,593761,525943,842480,535981,127048,524535&storeNumbers=2475"
+curl -s -o /home/rpr/coronacharts/fetchdm/data/${DATUM}.json "https://products.dm.de/store-availability/DE/availability?dans=708997,853483,842480,524535,846834,524533,137425,28171,595420,452753,879536,452744,504606,593761,535981,127048,524532,711915,485698,485695,525943,608795&storeNumbers=2475"
 for i in $(cat /home/rpr/coronacharts/fetchdm/data/${DATUM}.json | jq '.storeAvailabilities' | jq 'to_entries[]' | jq '.[] | arrays | .[] ' | jq '.stockLevel');
 do
   # let X=$X+$i;
